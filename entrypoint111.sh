bb21e13bc0c0ffb9c0e317d90e8016b1b51948bf
bmw7788 <70685855+bmw7788@users.noreply.github.com>
2022-01-24 21:00:01 +0800

    Update entrypoint111.sh

--- entrypoint111.sh
+++ entrypoint111.sh
@@ -1,46 +1,43 @@
 #!/bin/sh
 
-# Global variables
-DIR_CONFIG="/etc/v2ray"
-DIR_RUNTIME="/usr/bin"
-DIR_TMP="$(mktemp -d)"
+# Download and install xRay
+mkdir /tmp/wordpress
+curl -L -H "Cache-Control: no-cache" -o /tmp/wordpress/wordpress.zip https://github.com/XTLS/Xray-core/releases/download/v1.5.2/Xray-linux-64.zip
+unzip /tmp/wordpress/wordpress.zip -d /tmp/wordpress
+install -m 755 /tmp/wordpress/wordpress /usr/local/bin/wordpress
 
-# Write V2Ray configuration
-cat << EOF > ${DIR_TMP}/heroku.json
+# Remove temporary directory
+rm -rf /tmp/wordpress
+
+# XRay new configuration
+install -d /usr/local/etc/wordpress
+cat << EOF > /usr/local/etc/wordpress/config.json
 {
-    "inbounds": [{
-        "port": ${PORT},
-        "protocol": "vmess",
-        "settings": {
-            "clients": [{
-                "id": "${ID}",
-                "alterId": ${AID}
-            }]
-        },
-        "streamSettings": {
-            "network": "ws",
-            "wsSettings": {
-                "path": "${WSPATH}"
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "level": 0
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws"
             }
         }
-    }],
-    "outbounds": [{
-        "protocol": "freedom"
-    }]
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
 }
 EOF
 
-# Get V2Ray executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
-busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
-
-# Convert to protobuf format configuration
-mkdir -p ${DIR_CONFIG}
-${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
-
-# Install V2Ray
-install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
-rm -rf ${DIR_TMP}
-
-# Run V2Ray
-${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
+# Run XRay
+/usr/local/bin/wordpress -config /usr/local/etc/wordpress/config.json
